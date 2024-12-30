a6527c7ebfc743db0579177bcdda86db02948025
Taras <45391115+prystupataras@users.noreply.github.com>
2024-12-30 16:40:49 +0200

    Initial Commit

---
+++ SnapshotSwiftUI/SnapshotSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapshotSwiftUIApp.swift
+//  SnapshotSwiftUI
+//
+//  Created by Taras Prystupa on 30.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SnapshotSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
