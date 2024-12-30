d62c4e0dbc247be20c1845905dd914f343c914b2
Taras <45391115+prystupataras@users.noreply.github.com>
2024-12-30 17:10:38 +0200

    snapshot view modifier
    can be used for all type views to create image from it.

--- SnapshotSwiftUI/ContentView.swift
+++ SnapshotSwiftUI/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var trigger: Bool = false
+    @State private var snapshot: UIImage?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(0...20, id: \.self) {
+                    Text("List Cell \(Int($0))")
+                }
+            }
+            .navigationTitle("List View")
+            .toolbar {
+                ToolbarItem {
+                    Button("Take Snapshot") {
+                        trigger.toggle()
+                    }
+                }
+            }
         }
-        .padding()
+        .snapshot(trigger: trigger) {
+            snapshot = $0
+        }
+        .ignoresSafeArea()
+        .overlay {
+            if let  snapshot {
+                Image(uiImage: snapshot)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(.rect(cornerRadius: 10))
+                    .padding(15)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background {
+                        Rectangle()
+                            .fill(.black.opacity(0.3))
+                            .ignoresSafeArea()
+                            .onTapGesture {
+                                self.snapshot = nil
+                            }
+                    }
+            }
+        }
     }
 }
 
